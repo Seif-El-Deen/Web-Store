67d2953e74f7f201d03c320bc0740e71c99cd815
Seif Mostafa <67309639+Seif-Mustafa@users.noreply.github.com>
2025-08-03 21:52:08 +0300

    Update top_customers_with_total_spending.sql

--- Scripts/Reports/top_customers_with_total_spending.sql
+++ Scripts/Reports/top_customers_with_total_spending.sql
@@ -1,9 +1,10 @@
 -- SQL Query to Find the top customers by total spending.
 
-select CONCAT(c.cust_fname,' ', c.cust_lname) as customer_name,  sum(o.ord_total) as total_spending
-FROM customer c
-inner join orders o
-on c.cust_id = o.ord_cust_id
--- where c.cust_id <=500000 -- to limit for only 500 k users
-group by  CONCAT(c.cust_fname,' ', c.cust_lname)
-order by 2 desc;
+WITH GROUPED_ORDERS AS(
+select o.ord_cust_id as customer_ID,  sum(o.ord_total) as total_spending
+FROM orders o
+group by  o.ord_cust_id)
+SELECT CONCAT(c.cust_fname,' ', c.cust_lname) as customer_name, go.total_spending
+FROM GROUPED_ORDERS go 
+INNER JOIN customer c
+on c.cust_id = go.customer_ID;
