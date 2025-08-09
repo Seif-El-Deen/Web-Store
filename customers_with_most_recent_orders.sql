c6694c054dfb928d3abf2485092699f746f43606
Seif Mostafa <67309639+Seif-Mustafa@users.noreply.github.com>
2025-08-09 11:53:44 +0300

    Delete customers_with_most_recent_orders.sql

--- customers_with_most_recent_orders.sql
+++
@@ -1,8 +0,0 @@
--- SQL Query to Retrieve the most recent orders with customer information with 1000 orders.
-
-select CONCAT(c.cust_fname,' ', c.cust_lname) customer_name, o.ord_id order_id, o.ord_date order_date
-FROM customer c
-inner join orders o
-on c.cust_id = o.ord_cust_id
-order by o.ord_date desc
-limit 1000;
